686fc9fd49b36dc5bdd4f5100637ea0e8a436be2
Victor Gomes <victor.nnjg@gmail.com>
2017-11-01 20:03:19 -0200

    Commit inicial da aplicação FrontEnd

---
+++ src/main/webapp/course-suggestion-spa/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p>Dê sugestões nos cursos do IFTM de Uberaba. Para começar insira seu nome e-mail abaixo:</p>
+    <form>
+      <input v-model="name" placeholder="Nome"><br>
+      <input v-model="email" placeholder="E-mail"><br>
+      <button>Avançar</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Sugestões para Cursos',
+      institutionImage: {
+        src: '',
+        alt: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
