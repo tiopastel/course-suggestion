138eeae6f155646f8c12db5e2381aa27c6e6c0c5
Victor Gomes <victor.nnjg@gmail.com>
2017-11-04 18:25:02 -0200

    Início da construção da página inicial

--- src/main/webapp/course-suggestion-spa/src/App.vue
+++ src/main/webapp/course-suggestion-spa/src/App.vue
@@ -2,10 +2,11 @@
   <div>
     <h1>{{ title }}</h1>
     <p>Dê sugestões nos cursos do IFTM de Uberaba. Para começar insira seu nome e e-mail abaixo:</p>
-    <form>
-      <input v-model="name" placeholder="Nome"><br>
-      <input v-model="email" placeholder="E-mail"><br>
-      <button>Avançar</button>
+    <form action="?" method="POST">
+      <input v-model="name" placeholder="Nome"><br><br>
+      <input v-model="email" placeholder="E-mail"><br><br>
+      <div class="g-recaptcha" :data-sitekey="recaptcha.sig_key"></div><br><br>
+      <input type="submit" value="Avançar">
     </form>
   </div>
 </template>
@@ -14,6 +15,10 @@
 export default {
   data() {
     return {
+      recaptcha: {
+        sig_key: '6LfbITcUAAAAAJ07pND9XkGBEBCdstWCdcpVGNGx',
+        id: 0
+      },
       title: 'Sugestões para Cursos',
       institutionImage: {
         src: '',
@@ -21,6 +26,12 @@ export default {
       },
       name: '',
       email: ''
+    }
+  },
+  mounted() {
+    if (window.grecaptcha) {
+      this.recaptcha.id = grecaptcha.render( $('.g-recaptcha')[0], { sitekey : this.recaptcha.sig_key });
+      console.log('this.recaptcha.id -> '+this.recaptcha.id);
     }
   }
 }
