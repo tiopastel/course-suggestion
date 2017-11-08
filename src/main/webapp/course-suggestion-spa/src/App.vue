c9ad59f4eae82862ef5dabb06d45403065763aa5
Victor Gomes <victor.nnjg@gmail.com>
2017-11-08 00:07:17 -0200

    Finalização da criação da página inicial e início da criação da página de seleção de cursos.

--- src/main/webapp/course-suggestion-spa/src/App.vue
+++ src/main/webapp/course-suggestion-spa/src/App.vue
@@ -1,40 +1,22 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <p>Dê sugestões nos cursos do IFTM de Uberaba. Para começar insira seu nome e e-mail abaixo:</p>
-    <form action="?" method="POST">
-      <input v-model="name" placeholder="Nome"><br><br>
-      <input v-model="email" placeholder="E-mail"><br><br>
-      <div class="g-recaptcha" :data-sitekey="recaptcha.sig_key"></div><br><br>
-      <input type="submit" value="Avançar">
-    </form>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      recaptcha: {
-        sig_key: '6LfbITcUAAAAAJ07pND9XkGBEBCdstWCdcpVGNGx',
-        id: 0
-      },
-      title: 'Sugestões para Cursos',
-      institutionImage: {
-        src: '',
-        alt: ''
-      },
-      name: '',
-      email: ''
-    }
-  },
-  mounted() {
-    if (window.grecaptcha) {
-      this.recaptcha.id = grecaptcha.render( $('.g-recaptcha')[0], { sitekey : this.recaptcha.sig_key });
-      console.log('this.recaptcha.id -> '+this.recaptcha.id);
+  export default {
+    data() {
+      return {
+        title: 'Sugestões para Cursos',
+        institutionImage: {
+          src: '',
+          alt: ''
+        }
+      }
     }
   }
-}
 </script>
 
 <style>
--- src/main/webapp/course-suggestion-spa/src/main.js
+++ src/main/webapp/course-suggestion-spa/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VeeValidate from 'vee-validate';
+import VueRouter from 'vue-router';
+import { messages } from '../node_modules/vee-validate/dist/locale/pt_BR.js';
+import { routes } from './routes';
+
+Vue.use(VeeValidate, {
+  locale: 'pt_BR',
+  dictionary: { 
+    pt_BR: { messages }
+  }
+});
+Vue.use(VueRouter);
+
+const router = new VueRouter({ routes, mode: 'history' });
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+  router
 })
